e85d83fb3a323ac787f5fb3f5830418468d944bc
Niraj Patidar <nirajpatidar11@gmail.com>
2020-04-01 09:24:55 +0530

    remove https and use http

--- src/Services/authService.js
+++ src/Services/authService.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const authLogin = (values) => {
     console.log('values', values)
-    return axios.post('https://node-2020.herokuapp.com/login', values);
+    return axios.post('http://node-2020.herokuapp.com/login', values);
 }
